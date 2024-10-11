5025239dd940233a286fde53d4496a7717e541dc
your_name <your_email@example.com>
2024-10-10 20:02:12 -0700

    C

--- contracts/Deal.sol
+++ contracts/Deal.sol
@@ -1,16 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
-contract Dealing{
-    mapping(address=>uint256)internal Balance;
-    mapping(uint256=>Deal)public Deals;
+contract Dealing {
+    mapping(address => uint256) internal Balance;
+    mapping(uint256 => Deal) public Deals;
     uint CurrentId;
-    enum Status{Initial,DealAdd,SellerAgree,BuyerSend,SellerReceive,BuyerSatisfy,SellerSatisfy,DealDone}
-    struct Deal{
+    enum Status {
+        Initial,
+        DealAdd,
+        SellerAgree,
+        BuyerSend,
+        SellerReceive,
+        BuyerSatisfy,
+        SellerSatisfy,
+        DealDone
+    }
+    struct Deal {
         uint id;
         uint dealamount;
         address buyer;
         address seller;
         Status dealstatus;
     }
-    
+    function Withdraw(uint amount) public payable {
+        require(Balance[msg.sender] >= amount, "Insuffient Balance");
+        Balance[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+    receive() external payable {
+        assert(msg.value > 0);
+        Balance[msg.sender] += msg.value;
+    }
 }
